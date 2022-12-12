ec257a86435713d7fe91d7a2cd114ad6a90abdd5
Mukul Gupta <guptamukul@google.com>
2022-12-11 21:53:45 -0800

    Update main.tf

--- modules/secure_data_warehouse/main.tf
+++ modules/secure_data_warehouse/main.tf
@@ -118,10 +118,7 @@ resource "google_artifact_registry_repository_iam_member" "confidential_python_r
 }
 
 
-// The sample data we are using is a Public Bigquery Dataset Table
-// that contains a United States Internal Revenue Service form
-// that provides the public with financial information about a nonprofit organization
-// (https://console.cloud.google.com/marketplace/product/internal-revenue-service/irs-990?project=bigquery-public-data)
+// The sample data we are using is a Mock Drivers License data
 module "regional_deid_pipeline" {
   source = "GoogleCloudPlatform/secured-data-warehouse/google//modules/dataflow-flex-job"
 
@@ -137,7 +134,6 @@ module "regional_deid_pipeline" {
   staging_location        = "gs://${module.secured_data_warehouse.data_ingestion_bucket_name}/staging/"
 
   parameters = {
-    # query                          = "SELECT ein, name, ico, street, city, state, income_amt, revenue_amt FROM [bigquery-public-data:irs_990.irs_990_ein] LIMIT 10000"
     query                          = "SELECT email, name, street, city, state, zip, dob, dl_id, exp_date FROM [${module.project_radlab_sdw_data_ingest.project_id}:${module.sdw_data_ingest_bq_dataset.bigquery_dataset.dataset_id}.${module.sdw_data_ingest_bq_dataset.external_table_ids[0]}] LIMIT 10000"
     deidentification_template_name = module.de_identification_template.template_full_path
     window_interval_sec            = 30
@@ -158,33 +154,34 @@ resource "time_sleep" "wait_de_identify_job_execution" {
   ]
 }
 
-module "regional_reid_pipeline" {
-  source = "GoogleCloudPlatform/secured-data-warehouse/google//modules/dataflow-flex-job"
+# module "regional_reid_pipeline" {
+#   source = "GoogleCloudPlatform/secured-data-warehouse/google//modules/dataflow-flex-job"
 
-  project_id              = module.project_radlab_sdw_conf_data.project_id
-  name                    = "dataflow-flex-regional-dlp-reid-job-python-query"
-  container_spec_gcs_path = module.template_project.python_re_identify_template_gs_path
-  job_language            = "PYTHON"
-  region                  = var.region
-  service_account_email   = module.secured_data_warehouse.dataflow_controller_service_account_email
-  subnetwork_self_link    = module.dwh_networking_conf.subnets_self_links[0]
-  kms_key_name            = module.secured_data_warehouse.cmek_reidentification_crypto_key
-  temp_location           = "gs://${module.secured_data_warehouse.confidential_data_dataflow_bucket_name}/tmp/"
-  staging_location        = "gs://${module.secured_data_warehouse.confidential_data_dataflow_bucket_name}/staging/"
+#   project_id              = module.project_radlab_sdw_conf_data.project_id
+#   name                    = "dataflow-flex-regional-dlp-reid-job-python-query"
+#   container_spec_gcs_path = module.template_project.python_re_identify_template_gs_path
+#   job_language            = "PYTHON"
+#   region                  = var.region
+#   service_account_email   = module.secured_data_warehouse.dataflow_controller_service_account_email
+#   subnetwork_self_link    = module.dwh_networking_conf.subnets_self_links[0]
+#   kms_key_name            = module.secured_data_warehouse.cmek_reidentification_crypto_key
+#   temp_location           = "gs://${module.secured_data_warehouse.confidential_data_dataflow_bucket_name}/tmp/"
+#   staging_location        = "gs://${module.secured_data_warehouse.confidential_data_dataflow_bucket_name}/staging/"
 
-  parameters = {
-    input_table                    = "${module.project_radlab_sdw_non_conf_data.project_id}:${local.non_confidential_dataset_id}.${local.non_confidential_table_id}"
-    deidentification_template_name = module.de_identification_template.template_full_path
-    window_interval_sec            = 30
-    batch_size                     = 1000
-    dlp_location                   = var.region
-    dlp_project                    = module.project_radlab_sdw_data_govern.project_id
-    bq_schema                      = local.bq_schema_dl
-    output_table                   = local.bigquery_confidential_table
-    dlp_transform                  = "RE-IDENTIFY"
-  }
-  depends_on = [
-    time_sleep.wait_de_identify_job_execution,
-    google_bigquery_table.re_id
-  ]
-}
+#   parameters = {
+#     input_table                    = "${module.project_radlab_sdw_non_conf_data.project_id}:${local.non_confidential_dataset_id}.${local.non_confidential_table_id}"
+#     deidentification_template_name = module.de_identification_template.template_full_path
+#     window_interval_sec            = 30
+#     batch_size                     = 1000
+#     dlp_location                   = var.region
+#     dlp_project                    = module.project_radlab_sdw_data_govern.project_id
+#     bq_schema                      = local.bq_schema_dl
+#     output_table                   = local.bigquery_confidential_table
+#     dlp_transform                  = "RE-IDENTIFY"
+#   }
+#   depends_on = [
+#     time_sleep.wait_de_identify_job_execution,
+#     google_bigquery_table.re_id,
+#     # google_project_iam_binding.sa_user
+#   ]
+# }
